35a80abde3bbc8c13971073df178291b265fc9c5
Yori Verbist <yori.verbist1212@gmail.com>
2019-05-09 18:24:39 +0200

    Klasse pretpark afgemaakt

--- src/java/fact/it/www/beans/Pretpark.java
+++ src/java/fact/it/www/beans/Pretpark.java
@@ -5,11 +5,54 @@
  */
 package fact.it.www.beans;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Yori Verbist
  * r0697651
  */
 public class Pretpark {
+    private String naam;
+    private int aantalBezoekers;
+    private ArrayList<Attractie> attracties = new ArrayList<>();
+
+    public Pretpark(String naam) {
+        this.naam = naam;
+    }
+
+    public String getNaam() {
+        return naam;
+    }
+
+    public void setNaam(String naam) {
+        this.naam = naam;
+    }
+
+    public ArrayList<Attractie> getAttracties() {
+        return attracties;
+    }
+    
+    public int getAantalAttracties() {
+        return attracties.size();
+    }
+    
+    public void voegAttractieToe(Attractie attractie){
+        attracties.add(attractie);
+    }
+
+    public Attractie zoekAttractieOpNaam(String naam) {
+        for(Attractie attractie: attracties){
+            if(attractie.getNaam().equals(naam)){
+                return attractie;
+            }
+        }
+        return null;
+    }
+    
+    public void registreerBezoeker(Bezoeker bezoeker){
+        aantalBezoekers ++;
+        bezoeker.setPretparkcode(aantalBezoekers);
+    }
     
 }
